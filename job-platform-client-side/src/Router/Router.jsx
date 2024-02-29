dec69c1133e1b30b1f1d9fe9039739af1e448674
cdJohnEl <easykelchimdikejohn@gmail.com>
2024-02-29 14:58:19 +0100

    Made landing page the homepage

--- job-platform-client-side/src/Router/Router.jsx
+++ job-platform-client-side/src/Router/Router.jsx
@@ -1,7 +1,7 @@
 import App from "../App";
 import Home from "../Pages/Home";
 import About from "../Pages/About";
-import {createBrowserRouter, } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import CreateJob from "../Pages/CreateJob";
 import Myjobs from "../Pages/Myjobs";
 import UpdateJob from "../Pages/UpdateJob";
@@ -10,40 +10,44 @@ import Signup from "../Components/Signup";
 import LandingPage from "../Pages/LandingPage";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 const router = createBrowserRouter([
-    {
-      path: "/landing-page",
-      element: <App/>,
-      children: [
-        { path: "/", element: <Home/> },
-        {
-          path: "/post-job",
-          element: <CreateJob/>
-        },
-        {
-          path: "/my-job",
-          element: <Myjobs/>
-        },
-        {
-          path: "/edit-job/:id",
-          element: <UpdateJob/>,
-          loader: ({params}) => fetch(`https://job-platform-ffzr.onrender.com/all-jobs/${params.id}`)
-        }
-      ],
-    },
-    {
-      path: "/landing-page",
-      element: <LandingPage/>,
-    },
-    {
-      path: "/login",
-      element: <Login/>,
-    },
-    {
-      path: "/sign-up",
-      element: <Signup/>,
-    }
-  ]);
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/home", element: <Home /> },
+      {
+        path: "/post-job",
+        element: <CreateJob />,
+      },
+      {
+        path: "/my-job",
+        element: <Myjobs />,
+      },
+      {
+        path: "/edit-job/:id",
+        element: <UpdateJob />,
+        loader: ({ params }) =>
+          fetch(`https://job-platform-ffzr.onrender.com/all-jobs/${params.id}`),
+      },
+    ],
+  },
+  // {
+  //   path: "/landing-page",
+  //   element: <LandingPage/>,
+  // },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/sign-up",
+    element: <Signup />,
+  },
+]);
 
-  export default router;
+export default router;
